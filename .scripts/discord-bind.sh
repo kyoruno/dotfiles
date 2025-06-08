80d8545f1504bfb3794695ecd98232fa3507ab2d
kyoruno <51477242+kyoruno@users.noreply.github.com>
2025-06-08 15:23:47 +0300

    updates

--- .scripts/discord-bind.sh
+++ .scripts/discord-bind.sh
@@ -9,21 +9,21 @@
 
 ###################################################################################
 
-if ! pidof vesktop > /dev/null; then 
-    vesktop & sleep 2 # Launch if its not running
+if ! ps aux | grep -v grep | grep legcord > /dev/null; then 
+    legcord & sleep 2 # Launch if its not running
 else
     json=$(hyprctl clients -j)
-    discord_client=$(echo "$json" | jq '.[] | select(.class == "vesktop")')
+    discord_client=$(echo "$json" | jq '.[] | select(.class == "legcord")')
     discord_wrkspc=$(echo "$discord_client" | jq -r '.workspace.name')
 
     # Most likely minimized to tray, this should bring it back
     if [ "$discord_client" == "" ]; then
-        vesktop &
+        legcord &
     fi
 
     # Move Disc back to its workspace if elsewhere
     if [ "$discord_wrkspc" != "special:discord" ]; then
-        hyprctl dispatch movetoworkspacesilent "special:discord,class:^(vesktop)$"
+        hyprctl dispatch movetoworkspacesilent "special:discord,class:^(legcord)$"
     fi
 fi
 
